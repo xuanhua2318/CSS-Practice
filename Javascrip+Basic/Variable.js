df2d14169e98d4912d281c366005e45daa89d8c6
xuanhua2318 <thuongxuan.2323@gmail.com>
2025-10-25 22:22:51 -0400

    javascript variable

---
+++ Javascrip+Basic/Variable.js
@@ -0,0 +1,38 @@
+let name = "Xuan Hua"
+age = "20"
+height = "6"
+
+// should use const by default is a good practice
+// only use yes if you know it would change
+const city_born = "Ho Chi Minh City";
+
+const name_1 = "Alice";
+const name_2 = "Ben";
+const name_3 = "Seko";
+
+const houses = "4";
+const people = "25";
+const building = "20";
+
+const isEnter = "true";
+const isEnter1 = "false";
+
+
+// empty
+const data = null;
+
+// undefined value
+let value;
+console.log(value);
+
+// array
+const colors = ["red", "green", "blue"];
+const numbers = ["1","2","3"];
+
+// object - collection of properties like json
+const person = {
+    name: "Ben",
+    age: "25",
+    height: "5.3",
+};
+
