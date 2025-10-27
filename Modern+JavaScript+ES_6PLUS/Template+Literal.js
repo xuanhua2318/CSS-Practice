5ad23c73e5cabedabdde4a861f48ec9d16544075
xuanhua2318 <thuongxuan.2323@gmail.com>
2025-10-27 15:05:45 -0400

    3.05pm

---
+++ Modern+JavaScript+ES_6PLUS/Template+Literal.js
@@ -0,0 +1,13 @@
+// MULTILINE STRING
+const multi_line_string = `
+    This is my first time coding in Javascript.
+    Learning is hard.
+`;
+
+console.log(multi_line_string);
+
+// EXPRESSION IN TEMPLATE LITERAL ${variable, expression}
+const a = 4;
+const b = 7;
+console.log(`Sum of a and b is:${a+b}`);
+console.log(`Is a > than b?${a>b}`);
